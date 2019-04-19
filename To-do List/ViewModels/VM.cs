037aee004fab396f829499dc2385c8c0f73a54bb
Fstrm <i.n3r@outlook.com>
2019-04-20 00:41:26 +0300

    Add UI

--- To-do List/ViewModels/VM.cs
+++ To-do List/ViewModels/VM.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using To_do_List.Models;
 
@@ -11,17 +8,30 @@ namespace To_do_List.ViewModels
 {
 	class VM
 	{
-		public ObservableCollection<TaskItem> TaskList { get; set; }
+		//public ObservableCollection<TaskItem> TaskList { get; set; }
+		public ObservableCollection<string> TaskList { get; set; }
 
-		public ICommand AddTask { get; }
+		public ICommand AddTask
+		{
+			get
+			{
+				return new RelayParametrizedCommand<string>(obj => TaskList.Add(obj));
+			}
+		}
 
-		public ICommand RemoveTask { get; }
+		public ICommand RemoveTask
+		{
+			get
+			{
+				return new RelayParametrizedCommand<int>(obj => TaskList.RemoveAt(obj));
+			}
+		}
 
 		public ICommand WriteToFile { get; }
 
 		public VM()
 		{
-			TaskList = new ObservableCollection<TaskItem>();
+			TaskList = new ObservableCollection<string>();
 		}
 	}
 }
